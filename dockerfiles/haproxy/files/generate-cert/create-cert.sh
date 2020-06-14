23713ad8851c69ebf3583cc49d37e5c51be322f6
Björn Bessert <bjoernbessert@users.noreply.github.com>
2020-06-14 21:31:03 +0200

    new cli method: first working version

--- dockerfiles/haproxy/files/generate-cert/create-cert.sh
+++ dockerfiles/haproxy/files/generate-cert/create-cert.sh
@@ -4,8 +4,10 @@ set -e
 set -u
 
 FQDN="${1}"
-HAPROXY_CERT_DIR='/etc/haproxy/certs/'
 BASEDIR="/opt/generate-cert/"
+HAPROXY_CERT_DIR='/etc/haproxy/certs/'
+HAPROXY_CRT_STORE='/etc/haproxy/certs'
+HAPROXY_SOCKET='/var/run/haproxy-back.sock'
 
 mkdir -p $BASEDIR/certs_tmp
 cd $BASEDIR/certs_tmp
@@ -20,4 +22,15 @@ cat ${FQDN}.crt ../ca.crt ${FQDN}.key > ${FQDN}.pem
 
 mv -f ${FQDN}.pem ${HAPROXY_CERT_DIR}
 
-/usr/bin/timeout 5 /usr/bin/supervisorctl restart haproxy_back
+#echo "Debug 1:"
+echo "${HAPROXY_CERT_DIR}${FQDN}.pem"
+
+# TODO: Why is thhis needed? Seems to active the socket or something
+echo "show info" | socat ${HAPROXY_SOCKET}  -
+
+echo "new ssl cert ${HAPROXY_CERT_DIR}${FQDN}.pem" | socat ${HAPROXY_SOCKET} -
+echo "show ssl cert" | socat ${HAPROXY_SOCKET} -
+echo -e "set ssl cert ${HAPROXY_CERT_DIR}${FQDN}.pem <<\n$(cat ${HAPROXY_CERT_DIR}${FQDN}.pem)\n" | socat ${HAPROXY_SOCKET} -
+echo "commit ssl cert ${HAPROXY_CERT_DIR}${FQDN}.pem" | socat ${HAPROXY_SOCKET} -
+echo "add ssl crt-list ${HAPROXY_CRT_STORE} ${HAPROXY_CERT_DIR}${FQDN}.pem" | socat ${HAPROXY_SOCKET} -
+
