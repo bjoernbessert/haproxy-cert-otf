7928e9edbb49784c38b0c71bcc058e1440dfc1d7
Björn Bessert <bjoernbessert@users.noreply.github.com>
2020-08-27 20:52:25 +0200

    Reduce TLS cert lifespan

--- dockerfiles/haproxy/files/generate-cert/create-cert.sh
+++ dockerfiles/haproxy/files/generate-cert/create-cert.sh
@@ -19,7 +19,7 @@ local_ca () {
 
   openssl req -subj "/CN=${FQDN}" -extensions v3_req -sha256 -new -key ${FQDN}.key -out ${FQDN}.csr
 
-  openssl x509 -req -set_serial "0x$(openssl rand -hex 16)" -extensions v3_req -days 825 -sha256 -in ${FQDN}.csr -CA ../ca.crt -CAkey ../ca.key -CAcreateserial -out ${FQDN}.crt -extfile <(sed "s/subjectAltName = placeholder/subjectAltName = DNS:${FQDN}/" ../server_cert.cnf)
+  openssl x509 -req -set_serial "0x$(openssl rand -hex 16)" -extensions v3_req -days 398 -sha256 -in ${FQDN}.csr -CA ../ca.crt -CAkey ../ca.key -CAcreateserial -out ${FQDN}.crt -extfile <(sed "s/subjectAltName = placeholder/subjectAltName = DNS:${FQDN}/" ../server_cert.cnf)
 
   cat ${FQDN}.crt ../ca.crt ${FQDN}.key > ${FQDN}.pem
 
